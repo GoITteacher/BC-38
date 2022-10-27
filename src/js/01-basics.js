abc100f97ceebb1b9f47a1abe77afcb8d8ef2a84
Пащенко Владимир <pikimelposhta@gmail.com>
2022-10-27 15:48:52 +0300

    'update'

--- src/js/01-basics.js
+++ src/js/01-basics.js
@@ -9,7 +9,7 @@
  * - Патерн "Ранне повернення"
  * - Function expression (функціональний вираз)
  */
-
+// ======================
 // function sum(x, y) {
 //   x *= x;
 //   y = y * y;
@@ -18,7 +18,58 @@
 // const result = sum(59, 12);
 // console.log(result);
 
+// function sum(x, y) {
+//   x = pow(x);
+//   y = pow(y);
+//   return x + y;
+// }
+
+// function pow(num) {
+//   return num * num;
+// }
+
+// console.log(1);
+// console.log(sum(10));
+// console.log(2);
+// ======================
+// function foo(x = '', y) {
+//   console.log(x, y);
+// }
+// foo();
+
+// ======================
+
+// function foo() {
+//   console.log(arguments);
+//     let sum = 0;
+//     for (let num of arguments) {
+//       sum += num;
+//     }
+//     console.log(sum);
+// }
+
+// foo([2, 4, 6, 25, 7, 3], [2, 4, 6, 25, 7, 3]);
+//====================
+
+// const arr = [1, 2, 4, 6, 1, 5, 8, 2, 7, 7, 1, 56, 1];
+
+// function find(x, arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === x) {
+//       return true;
+//     } else if (arr[i] > 50) {
+//       return 1;
+//     }
+//   }
+
+//   return false;
+// }
+
+// console.log(find(10, arr));
+
+// const res = foo();
+// console.log(res);
+
 /*
  * - Стек вызовов
- * - Stack trace и поиск ошибок
  */
